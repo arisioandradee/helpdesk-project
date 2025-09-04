a52f3512a825333ec337c7357959dd26807089aa
Arisio <arisiosaf@gmail.com>
2025-09-04 13:57:15 -0300

    Adding new files

---
+++ src/main/java/com/helpdeskturmaa/helpdesk/HelpdeskturmaaApplication.java
@@ -0,0 +1,22 @@
+package com.helpdeskturmaa.helpdesk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelpdeskturmaaApplication {
+
+
+    public static void main(String[] args) {
+    	
+    	
+        SpringApplication.run(HelpdeskturmaaApplication.class, args);
+        
+        System.out.println("**********************************************");
+        System.out.println("*                                            *");
+        System.out.println("*  APLICAÇÃO HELPDESK INICIADA COM SUCESSO! *");
+        System.out.println("*                                            *");
+        System.out.println("**********************************************");
+        
+    }
+}
