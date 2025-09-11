8ed4792a1e01586ff8140ef40e51f449b67cf0c6
Arisio <arisiosaf@gmail.com>
2025-09-11 14:40:15 -0300

    Removing old files

--- helpdesk/src/main/java/com/helpdeskturmaa/helpdesk/repositories/PessoaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.helpdeskturmaa.helpdesk.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.helpdeskturmaa.helpdesk.domain.Pessoa;
-
-public interface PessoaRepository extends JpaRepository<Pessoa, Integer>{
-
-}
--- helpdesk/src/main/java/com/helpdeskturmaa/helpdesk/repositories/TecnicoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.helpdeskturmaa.helpdesk.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.helpdeskturmaa.helpdesk.domain.Tecnico;
-
-public interface TecnicoRepository extends JpaRepository<Tecnico, Integer>{
-
-}
